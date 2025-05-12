662f23fd0cb93636b661fb6a4770437bd529286d
neilgabrielson <nagabrielson@gmail.com>
2025-05-12 16:02:52 -0400

    simplify laplacian for neumann conditions, rename laplacian_1D to double_derivative, update mori model

--- Code/pde_utils.py
+++ Code/pde_utils.py
@@ -1,16 +1,12 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 
-# laplacian function for periodic boundary conditions
-def periodic_laplacian(Z, DX=1.0):
+# approximates the laplacian with periodic boundaries
+def laplacian(Z, DX=1.0):
     return (np.roll(Z, 1, axis=1) + np.roll(Z, -1, axis=1) + np.roll(Z, 1, axis=0) + np.roll(Z, -1, axis=0) - 4*Z) / DX**2
 
-# laplacian function for neumann boundary conditions
-def neumann_laplacian(Z, DX=1.0):
-    return (Z[0:-2, 1:-1] + Z[1:-1, 0:-2] + Z[2:, 1:-1] + Z[1:-1, 2:] - 4*Z[1:-1, 1:-1]) / DX**2
-
-# 1D laplacian function for periodic boundary conditions
-def periodic_laplacian_1D(Z):
+# approximates the double derivative
+def double_derivative(Z):
     return np.roll(Z, 1) + np.roll(Z, -1) - 2*Z
 
 # generate correlated gaussian field
